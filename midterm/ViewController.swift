0b63119ae6d3d22f9702a0242852f99026f53fc5
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 09:32:02 -0500

    Initial Commit

---
+++ midterm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  midterm
+//
+//  Created by student on 2016-02-26.
+//  Copyright © 2016 Kelly MCalpine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
